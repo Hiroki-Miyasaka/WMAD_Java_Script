0f209efccbfc757efcec20e37ef10d7c0da9eb66
Hiroki-Miyasaka <h.m.jojo.8258@gmail.com>
2022-10-26 20:58:22 -0700

    finish until closed

---
+++ ex1/securityScript.js
@@ -0,0 +1,4 @@
+var sectionOneTitle = "<h3>closed-circui</h3>"
+var sectionOneDescription = "<p>Closed-circuit television (CCTV), also known as video surveillance, is the use</p>"
+document.write(sectionOneTitle)
+document.write(sectionOneDescription)
