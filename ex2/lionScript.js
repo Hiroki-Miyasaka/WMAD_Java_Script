7e486e020269f1f0936bc770a112d456f321c137
Hiroki-Miyasaka <h.m.jojo.8258@gmail.com>
2022-11-02 21:25:48 -0700

    modified

--- ex2/lionScript.js
+++ ex2/lionScript.js
@@ -1,5 +1,28 @@
+// var pictures = ["./pictures/lion1.png", "./pictures/lion2.png", "./pictures/lion3.png", "./pictures/lion4.png"]
+
+// for(let i = 0; i < pictures.length; i++){
+//     // document.getElementsById("images_area") => div
+//     // document.write() --> write this code for me in the html code
+//     // innerHTML
+//     let imageArea = document.getElementsById("images_area");
+//     let imageTag = `<img src="${pictures[i]}">`
+//     imageArea.innerHTML = imageTag; // <div>pictures[i];</div>
+// }
+
+// let firstName = "Hiroki"
+// let fullName = firstName + " Miyasaka"
+// let fullName1 = `${firstName} Miyasaka` // string interpolation
+// Java -> System.out.println("%s",firstName)
+
 var pictures = ["./pictures/lion1.png", "./pictures/lion2.png", "./pictures/lion3.png", "./pictures/lion4.png"]
 
-for(let i = 0; i < pictures.length; i++){
-    document.getElementsByClassName("images_area").innerHTML = pictures[i];
-}
+ for(let i = 0; i < pictures.length; i++){
+     // document.getElementsById("images_area") => div
+     // document.write() --> write this code for me in the html code
+     // innerHTML
+    //  let imageArea = document.getElementsById("images_area");
+    //  let imageTag = `<img src="${pictures[i]}">`
+    //  imageArea.innerHTML = imageTag; // <div>pictures[i];</div>
+    // document.getElementById("images_area").innerHTML = "hello";
+    document.getElementById("images_area").innerHTML = `<img src="${pictures[i]}">`;
+ }
