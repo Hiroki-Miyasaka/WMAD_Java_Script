a3908f195a29ffd52a12fa24babcbeeabf676cce
Hiroki-Miyasaka <h.m.jojo.8258@gmail.com>
2022-11-02 20:20:35 -0700

    modified

--- ex2/lionScript.js
+++ ex2/lionScript.js
@@ -1,4 +1,4 @@
-const pictures = ["./pictures/lion1.png", "./pictures/lion2.png", "./pictures/lion3.png", "./pictures/lion4.png"]
+var pictures = ["./pictures/lion1.png", "./pictures/lion2.png", "./pictures/lion3.png", "./pictures/lion4.png"]
 
 for(let i = 0; i < pictures.length; i++){
     document.getElementsByClassName("images_area").innerHTML = pictures[i];
