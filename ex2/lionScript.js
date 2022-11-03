8188755f5e7cd44e7580b6a48aeef96732a303bb
Hiroki-Miyasaka <h.m.jojo.8258@gmail.com>
2022-11-03 03:37:41 -0700

    modified

--- ex2/lionScript.js
+++ ex2/lionScript.js
@@ -13,16 +13,12 @@
 // let fullName = firstName + " Miyasaka"
 // let fullName1 = `${firstName} Miyasaka` // string interpolation
 // Java -> System.out.println("%s",firstName)
-
-var pictures = ["./pictures/lion1.png", "./pictures/lion2.png", "./pictures/lion3.png", "./pictures/lion4.png"]
+var pictures = ["./pictures/lion1.png", "./pictures/lion2.png", "./pictures/lion3.png", "./pictures/lion4.png"];
 
  for(let i = 0; i < pictures.length; i++){
-     // document.getElementsById("images_area") => div
-     // document.write() --> write this code for me in the html code
-     // innerHTML
-    //  let imageArea = document.getElementsById("images_area");
-    //  let imageTag = `<img src="${pictures[i]}">`
-    //  imageArea.innerHTML = imageTag; // <div>pictures[i];</div>
+    let imageArea = document.getElementById("images_area");
+    let imageTag = `<img src="${pictures[i]}">`;
+    imageArea.innerHTML = imageTag; 
     // document.getElementById("images_area").innerHTML = "hello";
-    document.getElementById("images_area").innerHTML = `<img src="${pictures[i]}">`;
+    // document.getElementById("images_area").innerHTML = `<img src=s${pictures[i]}>`;
  }
