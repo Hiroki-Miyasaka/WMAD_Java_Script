c873e5e1f38e2e920a1de41eac84744c5e45520e
Hiroki-Miyasaka <h.m.jojo.8258@gmail.com>
2022-11-03 16:40:39 -0700

    modified

--- ex2/lionScript.js
+++ ex2/lionScript.js
@@ -13,12 +13,40 @@
 // let fullName = firstName + " Miyasaka"
 // let fullName1 = `${firstName} Miyasaka` // string interpolation
 // Java -> System.out.println("%s",firstName)
-var pictures = ["./pictures/lion1.png", "./pictures/lion2.png", "./pictures/lion3.png", "./pictures/lion4.png"];
 
- for(let i = 0; i < pictures.length; i++){
-    let imageArea = document.getElementById("images_area");
-    let imageTag = `<img src="${pictures[i]}">`;
-    imageArea.innerHTML = imageTag; 
-    // document.getElementById("images_area").innerHTML = "hello";
-    // document.getElementById("images_area").innerHTML = `<img src=s${pictures[i]}>`;
- }
+
+const pictures = ["./pictures/lion1.png", "./pictures/lion2.png", "./pictures/lion3.png", "./pictures/lion4.png"];
+let imageArea = document.getElementById("images_area");
+let imageTag = "";
+
+for(let i = 0; i < pictures.length; i++){
+// //     let imageArea = document.getElementById("images_area");
+    imageTag += `<img src=${pictures[i]}>`;
+
+}
+console.log(imageArea);
+console.log(imageTag);
+
+imageArea.innerHTML = imageTag; 
+
+
+
+
+
+
+// const galleryImage = document.getElementsByClassName("gallery_image");
+// const Images = [
+//    "./pictures/lion1.png",
+//    "./pictures/lion2.png",
+//    "./pictures/lion3.png",
+//    "./pictures/lion4.png",
+//  ];
+
+// let image = "";
+// for (let i = 0; i < Images.length; i++) {
+//   image += `<img src=${Images[i]}>`;
+// }
+
+// galleryImage.innerHTML = image;
+// console.log(galleryImage);
+// console.log(image);
