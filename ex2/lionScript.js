5a2bf4968523277b167adfb926a643f5bffbf98f
Hiroki-Miyasaka <h.m.jojo.8258@gmail.com>
2022-11-03 21:24:05 -0700

    finish lion

--- ex2/lionScript.js
+++ ex2/lionScript.js
@@ -1,27 +1,9 @@
-// var pictures = ["./pictures/lion1.png", "./pictures/lion2.png", "./pictures/lion3.png", "./pictures/lion4.png"]
-
-// for(let i = 0; i < pictures.length; i++){
-//     // document.getElementsById("images_area") => div
-//     // document.write() --> write this code for me in the html code
-//     // innerHTML
-//     let imageArea = document.getElementsById("images_area");
-//     let imageTag = `<img src="${pictures[i]}">`
-//     imageArea.innerHTML = imageTag; // <div>pictures[i];</div>
-// }
-
-// let firstName = "Hiroki"
-// let fullName = firstName + " Miyasaka"
-// let fullName1 = `${firstName} Miyasaka` // string interpolation
-// Java -> System.out.println("%s",firstName)
-
-
-const pictures = ["./pictures/lion1.png", "./pictures/lion2.png", "./pictures/lion3.png", "./pictures/lion4.png"];
+const images = ["./pictures/lion1.png", "./pictures/lion2.png", "./pictures/lion3.png", "./pictures/lion4.png"];
 let imageArea = document.getElementById("images_area");
 let imageTag = "";
 
-for(let i = 0; i < pictures.length; i++){
-// //     let imageArea = document.getElementById("images_area");
-    imageTag += `<img src=${pictures[i]}>`;
+for(let i = 0; i < images.length; i++){
+    imageTag += `<img src=${images[i]}>`;
 
 }
 console.log(imageArea);
@@ -29,24 +11,6 @@ console.log(imageTag);
 
 imageArea.innerHTML = imageTag; 
 
-
-
-
-
-
-// const galleryImage = document.getElementsByClassName("gallery_image");
-// const Images = [
-//    "./pictures/lion1.png",
-//    "./pictures/lion2.png",
-//    "./pictures/lion3.png",
-//    "./pictures/lion4.png",
-//  ];
-
-// let image = "";
-// for (let i = 0; i < Images.length; i++) {
-//   image += `<img src=${Images[i]}>`;
-// }
-
-// galleryImage.innerHTML = image;
-// console.log(galleryImage);
-// console.log(image);
+function clickEvent(){
+    window.alert("lion");
+}
