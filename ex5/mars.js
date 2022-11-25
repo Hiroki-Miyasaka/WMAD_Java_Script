af63cff186dc8c7d387ba73de928509fc212d35a
Hiroki-Miyasaka <h.m.jojo.8258@gmail.com>
2022-11-24 19:45:44 -0800

    finish

--- ex5/mars.js
+++ ex5/mars.js
@@ -2,7 +2,7 @@
 
 let container = document.getElementById("destination_select")
 var allTheOptions = "";
-let planets = ["Mars", "Jupiter", "Mercury"];
+let planets = ["Mars", "Jupiter", "Mercury", "Saturn"];
 
 planets.forEach(value => {
     allTheOptions += `<option>${value}</option>`;
